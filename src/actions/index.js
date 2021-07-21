d6dac733cf6f649a3babbe42f4e4c3f1ca884ca5
subhrajyoti <subhrajyotisankarsau@gmail.com>
2021-07-21 23:31:58 +0530

    design redux-thunk

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,9 @@
+import jsonplaceholder from "../apis/jsonplaceholder";
+
+
 export const fetchposts = () =>{
-    return {
-        type:'FETCH_POSTS',
-    };
+   return async (dispatch,getState) => {
+       const response = await jsonplaceholder.get('/posts');
+       dispatch({type:'FETCH_POSTS',payload:response})
+   }  
 };
